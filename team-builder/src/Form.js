67cf189bbe95c4cc977f9193f4685a419d0783f3
simonecarolynr <simonecarolynr@gmail.com>
2020-01-24 12:28:18 -0700

    state-added

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { useState, setState } from 'react';
 
 function Form() {
+
+    const { user, setUser } = useState({
+        fName: "",
+        lName: "",
+        email: "",
+    });
+    
+    function onChange (event) {
+        setUser({...user, [event.target.name]: event.target.value})
+    };
+
     render(
         <form>
 
             <label>First Name: 
-                <input name='fName' type='text' />
+                <input name='fName' type='text' value={user.fName} onChange={onChange}/>
             </label>
 
             <label>Last Name: 
-                <input name='lName' type='text' />
+                <input name='lName' type='text' value={user.lName} onChange={onChange}/>
             </label>
 
             <label>Email: 
-                <input name='email' type='text' />
+                <input name='email' type='email' value={user.email} onChange={onChange}/>
             </label>
 
             <label>Position: 
@@ -22,6 +34,8 @@ function Form() {
                 <option value='2'>Front End</option>
                 <option value='3'>Designer</option>
             </label>
+
+            <button type="submit" />
 
         </form>
     )
--- team-builder/src/Members.js
+++ team-builder/src/Members.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useState, setState} from 'react';
 import { render } from 'react-dom';
+import Form from './Form';
 
-const [members, setMembers] = setState({
-
-});
+const { members, setMembers } = useState([]);
 
 function Members() {
     render(
